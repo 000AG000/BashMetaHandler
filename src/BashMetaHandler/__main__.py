06c8fa54a1d18fb8a76ea0ee0e69e840ced35744
000AG000 <416c657861@posteo.de>
2024-09-01 13:18:25 +0200

    another flask8 fix and requirments.txt

--- src/BashMetaHandler/__main__.py
+++ src/BashMetaHandler/__main__.py
@@ -5,7 +5,7 @@ import click
 @click.command()
 @click.version_option()
 def main() -> int:
-    """main function for testing
+    """Main function for testing.
 
     Returns:
         int: return 0 when properly finished
